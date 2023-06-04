1a010e94386be8c24ee007e8f23937c66cdf2191
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-06-04 01:07:48 +0100

    Removes `through2`

--- index.mjs
+++ index.mjs
@@ -1,54 +1,71 @@
-import path from 'path'
+import path from 'node:path'
+import {
+  Transform
+} from 'node:stream'
 import fancyLog from 'fancy-log'
-import through from 'through2'
 import tildify from 'tildify'
 import stringifyObject from 'stringify-object'
 import chalk from 'chalk'
 import plur from 'plur'
 
-const INDENT = String.fromCharCode(32).repeat(7)
+const WHITE_SPACE = String.fromCharCode(32)
 
 function getFull (file, verbose = false) {
   return (
-    (file.cwd ? 'cwd:'.padEnd(7, String.fromCharCode(32)) + chalk.blue(tildify(file.cwd)) : '') + '\n' +
-    (file.base ? 'base:'.padEnd(7, String.fromCharCode(32)) + chalk.blue(tildify(file.base)) : '') + '\n' +
-    (file.path ? 'path:'.padEnd(7, String.fromCharCode(32)) + chalk.blue(tildify(file.path)) : '') + '\n' +
-    (file.stat && verbose ? 'stat:'.padEnd(7, String.fromCharCode(32)) + chalk.blue(stringifyObject(file.stat, { indent: INDENT }).replace(/[{}]/g, '').trim()) : '')
+    (file.cwd ? 'cwd:'.padEnd(7, WHITE_SPACE) + chalk.blue(tildify(file.cwd)) : '') + '\n' +
+    (file.base ? 'base:'.padEnd(7, WHITE_SPACE) + chalk.blue(tildify(file.base)) : '') + '\n' +
+    (file.path ? 'path:'.padEnd(7, WHITE_SPACE) + chalk.blue(tildify(file.path)) : '') + '\n' +
+    (file.stat && verbose ? 'stat:'.padEnd(7, WHITE_SPACE) + chalk.blue(stringifyObject(file.stat, { indent: WHITE_SPACE.repeat(7) }).replace(/[{}]/g, '').trim()) : '')
   )
 }
 
-export default (options) => {
+const DEFAULT_OPTIONS = {
+  logger: fancyLog,
+  title: '@sequencemedia/gulp-debug:',
+  minimal: true,
+  showFiles: true,
+  showCount: true
+}
+
+const VERBOSE_OPTIONS = {
+  verbose: true,
+  minimal: false,
+  showFiles: true,
+  showCount: true
+}
+
+export default function gulpDebug (options = {}) {
   options = {
-    logger: fancyLog,
-    title: 'gulp-debug:',
-    minimal: true,
-    showFiles: true,
-    showCount: true,
+    ...DEFAULT_OPTIONS,
     ...options
   }
 
   if (process.argv.includes('--verbose')) {
-    options.verbose = true
-    options.minimal = false
-    options.showFiles = true
-    options.showCount = true
+    options = {
+      ...options,
+      ...VERBOSE_OPTIONS
+    }
   }
 
-  let count = 0
+  let COUNT = 0
 
-  return through.obj((file, encoding, done) => {
+  function transform (file, encoding, done) {
     if (options.showFiles) {
       options.logger(options.title + ' ' + (options.minimal ? chalk.blue(path.relative(process.cwd(), file.path)) : getFull(file, options.verbose)))
     }
 
-    count++
-
     done(null, file)
-  }, (done) => {
+  }
+
+  function flush (done) {
     if (options.showCount) {
-      options.logger(options.title + ' ' + chalk.green(count + ' ' + plur('item', count)))
+      options.logger(options.title + ' ' + chalk.green(COUNT + ' ' + plur('item', COUNT)))
     }
 
     done()
-  })
+  }
+
+  const stream = new Transform({ transform, flush, objectMode: true })
+
+  return stream.on('data', () => { COUNT++ })
 }
